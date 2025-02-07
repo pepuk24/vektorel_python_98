fa733f8f8768003890e49d2548f80ac9f14f6e94
pepuk24 <pepuk24@gmail.com>
2025-02-07 10:11:46 +0300

    Add files via upload

---
+++ MESAFE ÇEVİRİCİ UYGULAMASI/uzaklık Çevirici.py
@@ -0,0 +1,175 @@
+
+
+
+print("\033[34m                                                                      ")
+
+print("                               ╔══════════════════════════════════════════════╗")
+print("                               ║                                              ║")
+print("                               ║             MESAFE ÇEVIRICI                  ║")
+print("                               ║                                              ║")
+print("                               ╚══════════════════════════════════════════════╝")
+
+print("                               ╔══════════════════════════════════════════════╗")
+print("                               ║                                              ║") 
+print("                               ║                                              ║")
+print("                               ║         1-METRE ==>KILOMETRE                 ║")
+print("                               ║         2-KM    ==> METRE                    ║")
+print("                               ║         3-METRE ==> INÇ                      ║")
+print("                               ║         4-INÇ   ==> METRE                    ║")                             
+print("                               ║         5-KM    ==> MIL                      ║")
+print("                               ║         6-MIL   ==> KILOMETRE                ║")
+print("                               ║         7-FEET  ==> KILOMETRE                ║") 
+print("                               ║         8-KM    ==> FEET                     ║") 
+print("                               ║         9-YARD  ==> KILOMETRE                ║") 
+print("                               ║         10-KM   ==> YARD                     ║")
+print("                               ║                                              ║")
+print("                               ║             SEÇIMINI YAP!!                   ║")
+print("                               ╚══════════════════════════════════════════════╝")
+
+input("                                      Lutfen bir rakam seçin...                \n")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+           
+                                               
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                         
+
+
+
+
+
+
+
+
+                                   
+
+                                                        
