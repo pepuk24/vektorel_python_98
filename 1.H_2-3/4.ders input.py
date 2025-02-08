3ac612a168bf7b2dfd95dfebc5f633f4ef7fac99
Remzi Demir <pepuk24@gmail.com>
2025-02-08 10:57:42 +0300

    Add files via upload

---
+++ 1.H_2-3/4.ders input.py
@@ -0,0 +1,203 @@
+"""
+
+#####################################################################
+#******************************************************************##
+# Python Öğreniyorum ##
+# Python 3 ##
+#******************************************************************##
+#####################################################################
+
+sa1=14
+sa2=33
+sonuc=sa1+sa2
+print (sonuc)
+
+print ("______________________________________________________")
+x=int("33")
+y=(91)
+z=x+y
+print (z)
+
+a=int("33")
+b=50
+c=a+b
+print(c)
+
+
+name=input("Adinizi giri:")
+print("merhaba, " + name)
+
+
+name=input("oturdugunuz ili girin\t:",)
+print("sistemize hoþ geldiniz..." + name)
+
+
+toplam=500
+borc=  int(input("Lutfen odemenizi yaziniz: "))
+#borc=  int(input("Lutfen odemenizi yaziniz: "))
+sonuc=toplam-borc
+print ("kalan borç:", sonuc)
+
+
+age=int(input("Yaþinizi giriniz: "))
+print("girdiginiz yaþ:"   str(age))
+
+
+#STR SAYIYA ÇEVIRME
+
+#1.YONTEM
+not1=input ("1.sinav nedir:")
+print(int(not1)+2)
+
+
+# 2.yontem
+
+not2=int (input("2.sinav nedir:"))
+print (not2 * 2)
+
+
+
+yas=int(input("Kaç yasindasin:\t"))
+print("demek 5 yil sonra ", yas+5, "yasinda olacaksin")
+
+
+s1=int(input("birinci sayiyi gir:\t"))
+s2=int(input("ikinci sayiyi gir:\t"))
+print ("Toplam:",s1+s2) 
+
+
+kilo= int(input("kaç kilosun?"))
+print("demek iki kati",kilo*2)
+
+
+dogum=int(input("Dogdugun yili yaz\t?"))
+yil=2025
+print ("DEMEK YASINDASINIZ\t:", yil-dogum)
+s1=int(input("Lutfen bir sayi giriniz\t:"))
+print("girdiginiz sayinin karesi\t:",s1**2)
+
+work=int(input("Kaç saat çalistiniz\t:"))
+saat=100
+print ("TOPLAM ALMANIZ GEREKEN KAZANÇ\t:",saat*work,"TL")
+
+
+KN1= int(input("dikdortgenin bir kenarini yaziniz\t:"))
+KN2= int(input("diger kenar uzunlugunu giriniz\t:"))
+çevre=2 * (KN1+KN2)
+print("dikdorgenin karesi\t:",çevre)
+
+
+km=int(input("Km gir\t:"))
+mt=km*100
+print("MESAFE",mt, "metre eder..")
+
+
+s1=int(input("1.snv gir\t:"))
+s2=int(input("2.snv gir\t:"))
+ortalama=(s1+s2) / 2
+print("ortalamniz",(ortalama))
+#; kullanimi
+
+print("merhaba");print("nasilsin");print("arkadasim")
+
+#sep ve end kullanimi
+print(*range(60,100,5),sep="-")
+
+print(*range(50,70,5),end="-- ile biter")
+
+
+
+print(*range(4,12,3))
+
+xx="aptikDemir12"
+a=xx
+b=a
+c=b
+print(len(c))
+
+
+
+bu
+bir
+denemedir
+yorumlama
+için
+
+
+sayi1=5
+sayi2=("3")
+print(sayi1*sayi2)
+piSayisi=3.14
+print ('pi sayısı=', piSayisi)
+
+print("_________________________________________________________")
+                          #input kullankmi
+#input()  #fonksiyonu, kullanıcıdan bir girdi bekler. Ancak, bu girdi bir değişkene atanmadığı için herhangi bir işleme tabi tutulmaz ve kaybolur.
+
+
+
+input ()
+not1=50
+print(not1*2)
+not1=input() #Girdiyi note1 isimli kutuya içine atti...
+print(not1*2)
+input()
+print("merhaba",not1)
+
+
+isim=input("Adini yaz:")
+yas=20
+print("Yaþinþz baþlangiçta",yas)
+yas=yas+5
+print("Yasiniz 5 yil sonra:", yas)
+yas=int(input("Yeni yaþi gir:"))
+print("yeni yaþiniz 10 ile çarpildiginda:",yas * 10)
+print("merhaba",isim,"yaþiniz",yas*10)
+
+Nav=input("Kereme xwe nave xwe nivis bike:")
+print("Slav",Nav)
+
+
+deger=int (input("bir sayi girin:"))
+            
+print("girdiginiz sayi",deger**2) 
+ad=input("Adinizi yaziniz\t:")
+yas=int(input("yaþinizi girin\t:"))
+sehir=input("þehir\t:")
+print(f"merhaba,Adiniz {ad} {yas} yaþinda ve {sehir} sehrinde yasiyorsunuz")
+
+
+#Girilen stringi sayiya çevirme
+#1.Yontem
+
+not1=input("1.sinavi gir\t:")    #======>1010 olur sonuç.. girdi 10 olarak dusunulmus
+print(int(not1*2))
+
+not1=input("1.sinavi gir\t:")    
+print(int(not1)*2)             #======>20 olur sonuç......girdi 10 olarak dusunulmus
+
+
+#2.Yontem
+
+not2=int(input("DEGER"))
+print((not2-10)*2)
+
+nt1=int(input("Deger girin:\n   "))
+nt2=int(input("deger gir:\n   "))
+ort=(nt1+nt2) / 2
+print("Ortalamaniz:\n" ,ort)
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
