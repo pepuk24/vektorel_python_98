c2c86d6806b6cd778649b30c522937be39301f06
pepuk24 <pepuk@yahoo.com>
2025-02-15 17:41:13 +0300

    def_tanimları_devam

---
+++ 2.H/2.hafta_total_tekrarlar.py
@@ -0,0 +1,63 @@
+# def merhaba():
+#     print("merhaba")           #Basit fonksiyonlar.......
+#     print("nasılsın")
+# merhaba()
+
+
+
+
+#_____________________________ Fonksiyon çagırma________________________________________________
+
+# a=3
+# b=4
+# def merhaba():
+#     print("selam" *3)
+#     print("nasılsin")
+
+
+# def topla ():
+ 
+#     print("Toplami:" ,a+b)
+
+# merhaba()
+# merhaba()
+# topla()
+# merhaba()
+
+
+#_____________________________________Parametre alan fonksiyonlar_____________________________________
+
+# def  kare_bul(sayi):
+#      cikti="{} sayisinin karesi {} sayisidir"
+#      print(cikti  .format(sayi,sayi*3))
+
+# kare_bul(5)
+
+
+# def kare_bul(sayi):
+#     çikti=f"{sayi} sayisinin karesi {sayi**2} sayisidir...."
+#     print(çikti)
+
+
+# kare_bul(2)
+
+
+# def kare_bul(sayi):
+#     print(f"{sayi} sayisinin karesi {sayi**2} dir....")
+
+
+# kare_bul(10)
+
+
+
+
+#____________________________Deger donduren Fonksiyonlar_________________________________________________________
+
+
+
+# def topla(a,b):
+#     return a+b
+
+# aptik=topla(3,10)
+
+# print(topla(4,aptik))
