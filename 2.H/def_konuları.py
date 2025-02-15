3d92db224b9f3c45b136ecdbd746c82f950df58d
pepuk24 <pepuk@yahoo.com>
2025-02-15 16:18:24 +0300

    read.md_WHOAMI

--- 2.H/def_konuları.py
+++ 2.H/def_konuları.py
@@ -66,6 +66,139 @@
                               
 
 
-def topla(aa,bb):
-    return f"toplam:{aa+bb}"
-print(topla(3,5))
+# def topla(aa,bb):
+#     return f"toplam:{aa+bb}"
+# print(topla(3,5))
+
+
+# def selam_ver():
+#     print("merhaba")
+
+# selam_ver()
+
+
+# def selam_ver(): 
+#     print("merhaba")
+    
+# selam_ver() 
+
+# def selam_ver():
+#     print("merhaba")
+
+# selam_ver()
+# selam_ver()
+# selam_ver()
+
+# def selam_ver (isim):
+#     print(f"merhaba {isim}")
+
+          
+# selam_ver("mehmet")
+# selam_ver("ahmet")
+
+
+
+# def topla(sayi1,sayi2):
+#     sonuc=sayi1+sayi2
+#     print(f"sonuç: {sonuc}")
+
+
+
+# topla(3,5)
+# topla(10,120)
+
+
+
+
+# def topla (a,b):
+#     sonuc=a+b
+#     return sonuc
+
+
+
+# sonuc=topla(3, 5)
+# print(sonuc)
+
+
+# def kontrol(a,b):
+#     if a > 10:
+#         return "buyuk sayı"
+#     print("bu  satır sadece a 10 dan kucukse çalışır")
+
+# print(kontrol(9,5))
+
+
+# def islem_yap(a,b):
+#     return a+b,a*b
+# toplam,carpım=islem_yap(3,4)
+# print( "toplam:", toplam)
+# print("çarpım:",carpım)
+
+# def func():
+#     print("hello world1")
+#     print("hello world1")
+#     print("hello world3")
+#     print("hello world4")
+
+# func()
+    
+
+
+# def yazdir(str):
+#     print(f"hello),{str}")
+
+# yazdir("yukardaki arg bakıyor  aslinda")
+
+
+
+# def topla(num1,num2):
+#     print(num1+num2)
+
+# topla(5,8)
+# topla(44,22)
+# topla (22,88)
+
+
+
+# def kisi(strName,strAge,strJob):
+#     print("kisi adi: {},yasi: {}, meslegi: {}" .format(strName,strAge,strJob))
+
+
+
+# kisi('Burak',44,'bilisim')
+
+
+# def nm_pow(nm1,nm2=0):
+#     print(nm1**nm2)
+
+
+
+# nm_pow(2,)
+
+
+# def nm_pow(nm1,nm2):
+#      print(nm1**nm2)
+
+
+# print(type(nm_pow(2,4)))
+
+
+
+
+# def nm_pow(nm1,nm2):
+#      return(nm1**nm2)
+
+
+# print(nm_pow(2,6))
+
+
+# def get_gr_nm(nm1,nm2):
+    
+#     if nm1>nm2:
+#         return nm1
+#     elif nm2>nm1:
+#         return nm2
+#     else:
+#         return nm1
+    
+# print(get_gr_nm(69, 67))
